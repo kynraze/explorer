d24def34a6a69d77c98e0f683585df1bce8da8b0
Kynraze <107695211+kynraze@users.noreply.github.com>
2023-08-18 14:22:12 +0700

    Update ChainProfile.vue

--- src/layouts/components/ChainProfile.vue
+++ src/layouts/components/ChainProfile.vue
@@ -29,9 +29,9 @@ function changeEndpoint(item: Endpoint) {
           "
           class="capitalize whitespace-nowrap text-base font-semibold text-gray-600 dark:text-gray-200 hidden md:!block"
         >
-          #{{
+          {{
+            baseStore.latest?.block?.header?.chain_id ||
             chainStore.chainName ||
-            baseStore.latest?.block?.header?.height ||
             ''
           }}
         </div>
